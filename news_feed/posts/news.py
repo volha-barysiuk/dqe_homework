2eb7ac788b0414b5b24ef6ee9e70f7ea64e208c7
Volha_Barysiuk <olga.v.borisyuk@gmail.com>
2025-04-06 13:59:48 +0400

    Task 10: Add feature to write posts into DB

--- news_feed/posts/news.py
+++ news_feed/posts/news.py
@@ -8,6 +8,7 @@ from ..utils import validators
 #   - methods: formatting end lines, specific to News posts.
 class News(Post):
     post_type = 'News'
+    type_id = 1
     post_prompt = '\nEnter the city: '
 
     def __init__(self, text, city):
@@ -17,3 +18,21 @@ class News(Post):
     # Combine end line in format: "<City>, <Current date in required format>"
     def _add_end_line(self):
         return f'{self._city}, {self._cur_date.strftime('%d/%m/%Y %H.%M')}'
+
+    # Verify if post already exists in database
+    def is_stored_in_db(self, connection):
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM News WHERE text = ? AND city = ?', (self._text, self._city))
+            result = True if cursor.fetchone() is not None else False
+            return result
+
+    # Write post into database.
+    # The data is populated in two tables: the Posts table and the table for storing posts of required type, e.g. News
+    def store_in_db(self, connection):
+        with connection.cursor() as cursor:
+            cursor.execute('INSERT INTO Posts (type_id, create_date) VALUES (?, ?)', (self.type_id, self._cur_date))
+            cursor.execute("SELECT last_insert_rowid()")
+            post_id = cursor.fetchone()[0]
+            cursor.execute('INSERT INTO News (post_id, text, city) VALUES (?, ?, ?)', (post_id, self._text, self._city))
+            connection.commit()
+
--- news_feed/posts/private_ad.py
+++ news_feed/posts/private_ad.py
@@ -8,6 +8,7 @@ from ..utils import validators
 #   - Methods: date validation; formatting end lines, specific to Private Ad posts.
 class PrivateAd(Post):
     post_type = 'Private Ad'
+    type_id = 2
     post_prompt = '\nEnter expiration date (dd/mm/yyyy): '
 
     def __init__(self, text, exp_date):
@@ -19,3 +20,20 @@ class PrivateAd(Post):
         exp_date = self._exp_date.strftime('%d/%m/%Y')
         days_diff = (self._exp_date - self._cur_date).days
         return f'Actual until: {exp_date}, {days_diff} day{'' if days_diff == 1 else 's'} left'
+
+    # Verify if post already exists in database
+    def is_stored_in_db(self, connection):
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM PrivateAd WHERE text = ? AND expiry_date = ?', (self._text, self._exp_date))
+            result = True if cursor.fetchone() is not None else False
+            return result
+
+    # Write post into database.
+    # The data is populated in two tables: the Posts table and the table for storing posts of required type, e.g. PrivateAd
+    def store_in_db(self, connection):
+        with connection.cursor() as cursor:
+            cursor.execute('INSERT INTO Posts (type_id, create_date) VALUES (?, ?)', (self.type_id, self._cur_date))
+            cursor.execute("SELECT last_insert_rowid()")
+            post_id = cursor.fetchone()[0]
+            cursor.execute('INSERT INTO PrivateAd (post_id, text, expiry_date) VALUES (?, ?, ?)', (post_id, self._text, self._exp_date))
+            connection.commit()
